7dcb4c5067cf5f574f7e6dfb4ac9c989ccbea1fc
Ayush Goyal <ayushg1214@gmail.com>
2022-04-27 17:15:23 +0530

    feat: Add IP parser for analytics

---
+++ src/lib.ts
@@ -0,0 +1,29 @@
+
+import fetch from "node-fetch";
+import { sentenceCase } from "sentence-case";
+
+const { IP_INFO_TOKEN } = process.env;
+
+const getVisitorMessage = async ({ ip }: { ip: string }): Promise<string> => {
+    let text = "New visitor 🎉\n";
+    const ipInfo = await fetch(`https://ipinfo.io/${ip}/json?token=${IP_INFO_TOKEN}`).then(res => res.json()) as Record<string, string>;
+
+    const words = {
+        "ip": "IP",
+        "city": "City",
+        "region": "Region",
+        "country": "Country",
+        "timezone": "Timezone",
+        "postal": "Zip code",
+        "org": "ISP",
+        "loc": "Google maps: "
+    }
+
+    Object.entries(ipInfo).map(([key, value]) => {
+        text += `\n${words[key]?? sentenceCase(key)}: ${key === "loc" ? value = `[link](https://www.google.com/maps/place/${value.split(",")[0]},${value.split(",")[1]})` : value}`;
+    });
+
+    return text;
+}
+
+export { getVisitorMessage }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import fetch from "node-fetch";
 import 'dotenv/config'
+import { getVisitorMessage } from "./lib.js";
 
 const { CHAT_ID, BOT_KEY, TG_KEY } = process.env;
 const server = fastify()
@@ -9,12 +10,17 @@ server.get("/", async (request, reply) => {
   reply.send({ "status": true, "message": "You might want to head over to https://ayushgoyal.dev to see what's cooking!" })
 })
 
-server.post<{ Body: { secret: string } }>('/message', async (req, reply) => {
+server.post<{ Body: { secret: string, text: string, type?: "analytics", ip?: string } }>('/message', async (req, reply) => {
   if (!req.body.secret || req.body.secret !== TG_KEY) {
     reply.code(403).send({ error: true, message: 'Secret not provided or did not match.' });
   }
 
   const API_URL = `https://api.telegram.org/bot${BOT_KEY}/sendMessage`;
+
+  if (req.body.type === "analytics") {
+    if(typeof req.ip !== "string") reply.code(404).send({ error: true, message: 'IP address was not provided' });
+    req.body.text = await getVisitorMessage({ ip: req.body.ip });
+  }
 
   const body = {
     chat_id: CHAT_ID,
@@ -31,8 +37,9 @@ server.post<{ Body: { secret: string } }>('/message', async (req, reply) => {
     },
   });
 
-  reply.send({ 'error': false, message: 'success' });
+  reply.send({ 'error': false, message: 'Message delivered successfully' });
 });
+
 
 server.listen(process.env.PORT || 3000, (err) => {
   console.log("Server listening on port 3000");
